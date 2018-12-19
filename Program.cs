2a434879e0dcf1a7a2449a6771b1653f4488bdbd
Gabox360 <Gabox360@users.noreply.github.com>
2018-12-19 13:55:05 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,13 @@ namespace tryCatchBoi
             Console.WriteLine("enter a number");
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("enter a number");
-            try(){
+            int wrongyBoi;
+            try{
+                wrongyBoi = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch()
+            {
 
             }
             double num2 = Convert.ToDouble(Console.ReadLine());
