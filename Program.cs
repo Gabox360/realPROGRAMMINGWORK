e7ede38b5502edcbb832c29234ff47a980b48654
Gabox360 <Gabox360@users.noreply.github.com>
2018-12-19 12:23:17 -0500

    Created Github page
    this is the work to do later today

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchBoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a number");
+            try(){
+
+            }
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a number");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a number");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+            double total = num1 + num2 + num3 + num4;
+            Console.WriteLine("Total is: " + total);
+            
+                
+        }
+    }
+}
